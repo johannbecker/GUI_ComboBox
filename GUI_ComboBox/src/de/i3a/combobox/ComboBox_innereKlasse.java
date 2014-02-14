4118391cf9bf32ce23066c358773d14e1ed15311
johannbecker <lilli.johann.becker@gmail.com>
2014-02-14 09:41:04 +0100

    Event Menu vervollständigt.

--- GUI_ComboBox/src/de/i3a/combobox/ComboBox_innereKlasse.java
+++ GUI_ComboBox/src/de/i3a/combobox/ComboBox_innereKlasse.java
@@ -5,7 +5,11 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
+import javax.sound.midi.SysexMessage;
 import javax.swing.BoxLayout;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -14,6 +18,7 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
@@ -28,58 +33,88 @@ public class ComboBox_innereKlasse<E> extends JFrame {
 	private JComboBox<String> jcomboBox;
 	private JMenuItem jmiRot, jmiBlau, jmiGelb, jmiBeenden;
 	private JMenuBar jmb;
-	
 
 	public ComboBox_innereKlasse() {
 		this.setTitle("ComboBox");
 		this.setSize(300, 300);
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		this.setLocationRelativeTo(null);
-		
+
 		createMenu();
 		initComponents();
 		initEvents();
-		
+
 		this.setVisible(true);
 	}
 
 	private void initEvents() {
-		 MeinActionListener mal = new MeinActionListener();
-		 jbtnRot.addActionListener(mal);
-		 jbtnGelb.addActionListener(mal);
-		 jbtnBlau.addActionListener(mal);
-		 jradioRot.addActionListener(mal);
-		 jradioGelb.addActionListener(mal);
-		 jradioBlau.addActionListener(mal);
-		 jcomboBox.addActionListener(mal);
+		MeinActionListener mal = new MeinActionListener();
+		jbtnRot.addActionListener(mal);
+		jbtnGelb.addActionListener(mal);
+		jbtnBlau.addActionListener(mal);
+		jradioRot.addActionListener(mal);
+		jradioGelb.addActionListener(mal);
+		jradioBlau.addActionListener(mal);
+		jcomboBox.addActionListener(mal);
+		jmiBeenden.addActionListener(mal);
+		jmiRot.addActionListener(mal);
+		jmiBlau.addActionListener(mal);
+		jmiGelb.addActionListener(mal);
+		jmiBeenden.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				beenden();
+
+			}
+		});
+		this.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				beenden();
+			}
+			
+		});
+
 	}
 
-	private void createMenu(){
-		
+	private void createMenu() {
+
+		jmb = new JMenuBar();
+
 		jmiBeenden = new JMenuItem("Beenden");
 		jmiRot = new JMenuItem("Rot");
 		jmiBlau = new JMenuItem("Blau");
 		jmiGelb = new JMenuItem("Gelb");
-		
+
 		JMenu jmenuDatei = new JMenu("Datei");
 		jmenuDatei.add(jmiBeenden);
 
 		JMenu jmenuBearbeiten = new JMenu("Bearbeiten");
 		jmenuBearbeiten.add(jmiRot);
-		jmenuBearbeiten.add(jmiBlau);
 		jmenuBearbeiten.add(jmiGelb);
-		
-		jmb = new JMenuBar();
+		jmenuBearbeiten.add(jmiBlau);
 
 		jmb.add(jmenuDatei);
 		jmb.add(jmenuBearbeiten);
-		
+
 		this.setJMenuBar(jmb);
 	}
-	
+
+	private void beenden() {
+		int i = JOptionPane.showConfirmDialog(null,
+				"Wollen Sie wirlich beenden?", "Beenden?",
+				JOptionPane.YES_NO_OPTION);
+		if (i == JOptionPane.YES_OPTION) {
+			System.exit(NORMAL);
+		}
+
+	}
+
 	private void initComponents() {
 		c = this.getContentPane();
-				
+
 		jpNorth = new JPanel();
 		jcomboBox = new JComboBox<>();
 		jcomboBox.addItem("Rot");
@@ -116,25 +151,25 @@ public class ComboBox_innereKlasse<E> extends JFrame {
 		c.add(jpWest, BorderLayout.WEST);
 		c.add(jpCenter, BorderLayout.CENTER);
 		c.add(jpSouth, BorderLayout.SOUTH);
-		
+
 		jpCenter.setBackground(Color.RED);
 		jradioRot.setSelected(true);
 		jcomboBox.setSelectedItem("Rot");
 	}
 
-	 private class MeinActionListener implements ActionListener{
-	
-		 @Override
-		 public void actionPerformed(ActionEvent e) {
+	private class MeinActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
 			String actionCommand = e.getActionCommand();
 			if (actionCommand.equals("comboBoxChanged"))
-				 actionCommand = (String) jcomboBox.getSelectedItem();
-			switch (actionCommand){
+				actionCommand = (String) jcomboBox.getSelectedItem();
+			switch (actionCommand) {
 			case "Rot":
-				 jpCenter.setBackground(Color.RED);
-				 jradioRot.setSelected(true);
-				 jcomboBox.setSelectedItem("Rot");
-				 break;
+				jpCenter.setBackground(Color.RED);
+				jradioRot.setSelected(true);
+				jcomboBox.setSelectedItem("Rot");
+				break;
 			case "Gelb":
 				jpCenter.setBackground(Color.YELLOW);
 				jradioGelb.setSelected(true);
@@ -145,13 +180,13 @@ public class ComboBox_innereKlasse<E> extends JFrame {
 				jradioBlau.setSelected(true);
 				jcomboBox.setSelectedItem("Blau");
 				break;
-			default: 
-				 jpCenter.setBackground(Color.RED);
-				 jradioRot.setSelected(true);
-				 jcomboBox.setSelectedItem("Rot");
-			 break;
+			default:
+				jpCenter.setBackground(Color.RED);
+				jradioRot.setSelected(true);
+				jcomboBox.setSelectedItem("Rot");
+				break;
 			}
-	 	}
+		}
 	}
 
 	public static void main(String[] args) {
